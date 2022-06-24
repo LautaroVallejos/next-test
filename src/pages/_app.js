2e671ce403dfc36ac04f9e90e7f70ddde5f2d770
Lautaro Vallejos <lautaro.vallejosk4m@gmail.com>
2022-06-24 20:05:12 -0300

    first commit

---
+++ src/pages/_app.js
@@ -0,0 +1,8 @@
+// Importacion del tema de bootswatch
+import "bootswatch/dist/vapor/bootstrap.min.css"
+
+function MyApp ( {Component, pageProps} ) {
+    return <Component {...pageProps} />
+}
+
+export default MyApp;
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Layout from "../../components/Layout";
+
+const Index = () => (
+    <Layout>
+        <h1>Index it`s working</h1>
+    </Layout>
+)
+
+export default Index;
